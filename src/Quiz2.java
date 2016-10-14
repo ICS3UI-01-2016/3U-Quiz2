1b3c93c4698d2539c5b36c29f83cf615140c57a9
ANNT0773 <ANNT0773@HRH-IC0027335.wrdsb.ca>
2016-10-14 09:31:09 -0400

    quiz 2 done

--- src/Quiz2.java
+++ src/Quiz2.java
@@ -4,14 +4,14 @@
  * and open the template in the editor.
  */
 
-
 import becker.robots.City;
 import becker.robots.Direction;
+import becker.robots.IPredicate;
 import becker.robots.Robot;
 
 /**
  *
- * @author YOUR_NAME_HERE!!!!
+ * @author Taylor Ann!!!!
  */
 public class Quiz2 {
 
@@ -20,11 +20,21 @@ public class Quiz2 {
      */
     public static void main(String[] args) {
         // Can use Wall1.txt, Wall2.txt, or Wall3.txt
-        City kw = new City("Wall1.txt");
+        City kw = new City("Wall2.txt");
         Robot karel = new Robot(kw, 1, 1, Direction.WEST);
-        
+
         //program solutions coded under here
-       
+        while (!karel.frontIsClear()) {
+            karel.turnLeft();
+            while (karel.frontIsClear()) {
+                karel.move();
+                if (!karel.isBesideThing(IPredicate.aLight)) {
+                    karel.turnLeft();
+                    karel.turnLeft();
+                    karel.turnLeft();
+                    break;
+                }
+            }
+        }
     }
-    
 }
