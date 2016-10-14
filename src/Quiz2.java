f2ff76996b20cb40c028f482b0df9e1af639bfb3
micla1676 <micla1676@HRH-IC0027336.wrdsb.ca>
2016-10-14 09:20:25 -0400

    Completed Quiz 2

--- src/Quiz2.java
+++ src/Quiz2.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
 
-/**
- *
- * @author YOUR_NAME_HERE!!!!
+/**14/10/2016
+ * Quiz 2
+ * @author micla1676
  */
 public class Quiz2 {
 
@@ -20,11 +13,37 @@ public class Quiz2 {
      */
     public static void main(String[] args) {
         // Can use Wall1.txt, Wall2.txt, or Wall3.txt
-        City kw = new City("Wall1.txt");
+        City kw = new City("Wall3.txt");
         Robot karel = new Robot(kw, 1, 1, Direction.WEST);
         
         //program solutions coded under here
-       
+       //integer to count walls
+        int wallnumber = 0;
+        
+        //loop infinitely
+        while(true){
+        //if path is not clear, turn left, move, then turn right.
+            if(!karel.frontIsClear()){
+            //add 1 to wall number
+            wallnumber = wallnumber + 1;
+            //turn left
+            karel.turnLeft();
+            //move
+            karel.move();
+            //turn right
+            karel.turnLeft();
+            karel.turnLeft();
+            karel.turnLeft();
+            
+        }
+        //if path is clear, print the wallnumber, and break out of the loop.  
+        else{
+            //print wallnumber
+            System.out.println(wallnumber);
+            break;
+        }
+        }
     }
     
-}
+    }
+    
