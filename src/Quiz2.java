b7df49a0b82fb52910b75bf680c9f16879dab4c0
bulka4927 <bulka4927@HRH-IC0027325.wrdsb.ca>
2016-10-14 09:36:38 -0400

    Quiz2finished

--- src/Quiz2.java
+++ src/Quiz2.java
@@ -1,17 +1,17 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 import becker.robots.City;
 import becker.robots.Direction;
+import becker.robots.IPredicate;
 import becker.robots.Robot;
 
 /**
  *
- * @author YOUR_NAME_HERE!!!!
+ * @author Bulka
  */
 public class Quiz2 {
 
@@ -22,9 +22,29 @@ public class Quiz2 {
         // Can use Wall1.txt, Wall2.txt, or Wall3.txt
         City kw = new City("Wall1.txt");
         Robot karel = new Robot(kw, 1, 1, Direction.WEST);
-        
+
         //program solutions coded under here
-       
+
+        //if facing a wall turn left
+        while (!karel.frontIsClear()) {
+            karel.turnLeft();
+        }
+
+        //whille beside the wall move forward
+        while (karel.isBesideThing(IPredicate.aWall)) {
+            karel.move();
+
+            //when not by a wall turn right
+            if (!karel.isBesideThing(IPredicate.aWall)) {
+                karel.turnLeft();
+                karel.turnLeft();
+                karel.turnLeft();
+
+                break;
+
+            }
+
+        }
+
     }
-    
 }
