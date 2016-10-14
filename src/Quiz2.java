bded7ac3153ffb81dc0d68254f7979156e7fece4
shieg0688 <shieg0688@HRH-IC0027330.wrdsb.ca>
2016-10-14 09:33:01 -0400

    Gurveer Shienh Quiz 2

--- src/Quiz2.java
+++ src/Quiz2.java
@@ -4,14 +4,14 @@
  * and open the template in the editor.
  */
 
-
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.RobotSE;
 
 /**
  *
- * @author YOUR_NAME_HERE!!!!
+ * @author Gurveer Shienh
  */
 public class Quiz2 {
 
@@ -21,10 +21,24 @@ public class Quiz2 {
     public static void main(String[] args) {
         // Can use Wall1.txt, Wall2.txt, or Wall3.txt
         City kw = new City("Wall1.txt");
-        Robot karel = new Robot(kw, 1, 1, Direction.WEST);
-        
+        RobotSE karel = new RobotSE(kw, 1, 1, Direction.WEST);
+
         //program solutions coded under here
-       
+        //begin movement
+        while (!karel.frontIsClear()) {
+            karel.turnLeft();
+            karel.move();
+            //check for wall
+            while (karel.frontIsClear()) {
+                karel.turnRight();
+                //stop movement if wall is not found
+                if (karel.frontIsClear()) {
+                    break;
+                }
+
+
+            }
+        }
+
     }
-    
 }
