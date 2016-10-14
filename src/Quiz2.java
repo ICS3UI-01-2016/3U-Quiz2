df006443a80606e1b9a8e3b87768cb9993850e40
manok5757 <manok5757@HRH-IC0027323.wrdsb.ca>
2016-10-14 09:22:27 -0400

    This is Quiz 2 completed

--- src/Quiz2.java
+++ src/Quiz2.java
@@ -4,14 +4,14 @@
  * and open the template in the editor.
  */
 
-
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.RobotSE;
 
 /**
  *
- * @author YOUR_NAME_HERE!!!!
+ * @author mmanok5757
  */
 public class Quiz2 {
 
@@ -20,11 +20,18 @@ public class Quiz2 {
      */
     public static void main(String[] args) {
         // Can use Wall1.txt, Wall2.txt, or Wall3.txt
-        City kw = new City("Wall1.txt");
-        Robot karel = new Robot(kw, 1, 1, Direction.WEST);
-        
+        City kw = new City("Wall3.txt");
+        RobotSE karel = new RobotSE(kw, 1, 1, Direction.WEST);
+
         //program solutions coded under here
-       
+        // make karel check if there is a wall if fron of him
+        while (!karel.frontIsClear()) {
+            // if there is a wall, make karel turn left
+            karel.turnLeft();
+            // make karel move once to next street
+            karel.move();
+            // make him check if there is a wall to his right
+            karel.turnRight();
+        }
     }
-    
 }
