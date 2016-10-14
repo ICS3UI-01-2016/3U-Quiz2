5b60ea5570d92d0ff3d8a51abbedfe7bd5c9ab06
wangk9757 <wangk9757@HRH-IC0027320.wrdsb.ca>
2016-10-14 09:30:13 -0400

    Done

--- src/Quiz2.java
+++ src/Quiz2.java
@@ -8,10 +8,11 @@
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.RobotSE;
 
 /**
  *
- * @author YOUR_NAME_HERE!!!!
+ * @author Kevin Wang
  */
 public class Quiz2 {
 
@@ -21,10 +22,20 @@ public class Quiz2 {
     public static void main(String[] args) {
         // Can use Wall1.txt, Wall2.txt, or Wall3.txt
         City kw = new City("Wall1.txt");
-        Robot karel = new Robot(kw, 1, 1, Direction.WEST);
+        RobotSE karel = new RobotSE(kw, 1, 1, Direction.WEST);
         
         //program solutions coded under here
+        //get karel to do that thing where he looks if theres a wall to his right side
+         
+          while(!karel.frontIsClear()){
+           karel.turnLeft();
+          karel.move();
+              karel.turnRight();
+                  
+         
+          }
+        }
        
     }
     
-}
+
